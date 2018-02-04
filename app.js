59e839827d7ac5b177220f2c567d5703e4c17af7
Agnieszka Urbańska <agnieszka.urbanska@ymail.com>
2018-02-04 20:04:16 +0100

    "Kolejna wersja, dodany kod z CL"

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+var arrow = document.querySelector(".arrow-left, .arrow-right");
+
+arrow.addEventListener("click", function () {
+
+
+})
+
+
+
+
+
+
+});
+
+
+
+
+
+
